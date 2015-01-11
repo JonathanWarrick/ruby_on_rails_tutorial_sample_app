27f3bd88f0f7c2cd209f13123ee3e377bfce2ad7
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-01-11 10:20:25 -0500

    Add sessions controller. Add corresponding routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
+# Use bundle exec rake routes to show all routes
 Rails.application.routes.draw do
-  root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  get 'sessions/new'
+
+  root            g   'static_pages#home'
+  get 'help'      => 'static_pages#help'
+  get 'about'     => 'static_pages#about'
+  get 'contact'   => 'static_pages#contact'
+  get 'signup'    => 'users#new'
+  get 'login'     => 'sessions#new'
+  post 'login'    => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
